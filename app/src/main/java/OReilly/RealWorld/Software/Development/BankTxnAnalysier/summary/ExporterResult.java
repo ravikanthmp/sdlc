e1276814195944f3552a634497e45f805aa33e87
ravi <ravikanthmp123@gmail.com>
2021-12-07 22:19:00 -0800

    Exporters

---
+++ app/src/main/java/OReilly/RealWorld/Software/Development/BankTxnAnalysier/summary/ExporterResult.java
@@ -0,0 +1,9 @@
+package OReilly.RealWorld.Software.Development.BankTxnAnalysier.summary;
+
+public class ExporterResult {
+    String res;
+
+    public String getRes(){
+        return res;
+    }
+}
---
+++ app/src/main/java/OReilly/RealWorld/Software/Development/BankTxnAnalysier/summary/HtmlExporter.java
@@ -0,0 +1,22 @@
+package OReilly.RealWorld.Software.Development.BankTxnAnalysier.summary;
+
+public class HtmlExporter implements Exporter {
+    @Override
+    public ExporterResult export(SummaryStatistics summaryStatistics) {
+        String result = "<!doctype html>";
+        result += "<html lang='en'>";
+        result += "<head><title>Bank Transaction Report</title></head>";
+        result += "<body>";
+        result += "<ul>";
+        result += "<li><strong>The sum is</strong>: " + summaryStatistics.getSum() + "</li>";
+        result += "<li><strong>The average is</strong>: " + summaryStatistics.getAverage() + "</li>";
+        result += "<li><strong>The max is</strong>: " + summaryStatistics.getMax() + "</li>";
+        result += "<li><strong>The min is</strong>: " + summaryStatistics.getMin() + "</li>";
+        result += "</ul>";
+        result += "</body>";
+        result += "</html>";
+        ExporterResult exporterResult = new ExporterResult();
+        exporterResult.res = result;
+        return exporterResult;
+    }
+}
